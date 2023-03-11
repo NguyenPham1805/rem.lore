213b8075a780a5ff1641bda9e1ab6a3aa88f43fe
nguyen.pham <nguyen.pham@vnresource.org>
2023-03-11 10:49:30 +0700

    init rem.lore

---
+++ apps/rem/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'rl-root',
+  template: `<h1>Rem app is running !!!</h1>`,
+  styleUrls: ['./app.component.less']
+})
+export class AppComponent {
+  title = 'rem'
+}
